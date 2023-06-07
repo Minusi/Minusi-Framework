35b2dde8d165a92377f6552eba77f3cfc73cf6a0
Minusi <minusie357@gmail.com>
2023-06-07 12:24:38 +0900

    [VSIX] vsix 프로젝트 기초 설정

---
+++ MInusiFramework/Minusix/Commands/MyCommand.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace Minusix
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            TestDialog dialog = new TestDialog();
+            dialog.ShowDialog();
+        }
+    }
+}
